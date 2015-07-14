a6b51cbcff838ed1d193a7c89bcbe1993e6aa1d5
nnkhuong <nguyenngockhuong@gmail.com>
2015-07-15 06:12:55 +0700

    commit id 2: adding countEvenNode for BST and UnitTest

---
+++ src/test/BinarySearchTreeTest.java
@@ -0,0 +1,67 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import dataStructure.BinarySearchTree.BinarySearchTree;
+
+public class BinarySearchTreeTest {
+
+	@Test
+	public void testBuildBinaryTree()
+	{
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		array.add(30);
+		array.add(23);
+		array.add(45);
+		array.add(6);
+		array.add(47);
+		array.add(50);
+		array.add(29);
+		array.add(10);
+		BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+		bst.buildBinaryTree(array);
+		
+		assertEquals(8, bst.countNodes());
+	}
+	
+	@Test
+	public void testCountLeaves()
+	{
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		array.add(30);
+		array.add(23);
+		array.add(45);
+		array.add(6);
+		array.add(47);
+		array.add(50);
+		array.add(29);
+		array.add(10);
+		BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+		bst.buildBinaryTree(array);
+		
+		assertEquals(3, bst.countLeaves());
+	}
+	
+	@Test
+	public void testCountEvenNode()
+	{
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		array.add(30);
+		array.add(23);
+		array.add(45);
+		array.add(6);
+		array.add(47);
+		array.add(50);
+		array.add(29);
+		array.add(10);
+		BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+		bst.buildBinaryTree(array);
+		
+		assertEquals(4, bst.countEvenNode());
+	}
+
+}

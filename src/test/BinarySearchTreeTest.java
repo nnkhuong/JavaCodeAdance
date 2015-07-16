40008d9e09f0ffd166055c7b3660f8e25d2e6a67
nnkhuong <nguyenngockhuong@gmail.com>
2015-07-16 10:05:48 +0700

    commit id 5: adding min,max of BST, min of LinkList

--- src/test/BinarySearchTreeTest.java
+++ src/test/BinarySearchTreeTest.java
@@ -95,12 +95,99 @@ public class BinarySearchTreeTest {
 		
 		ArrayList<Integer> result = new ArrayList<Integer>();
 		bst.collectEvenNodes(result);
-		System.out.println("---------------------");
+		
 		for(Integer evenNode : result)
 		{
-			System.out.println(evenNode);
-		}
-		//assertEquals(4, bst.getHeightOfTree());		
+			assertTrue(evenNode % 2 == 0);
+		}	
 	}
-
+	@Test
+	public void testIsAllEvenNodes()
+	{
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		array.add(30);
+		array.add(23);
+		array.add(45);
+		array.add(6);
+		array.add(47);
+		array.add(50);
+		array.add(29);
+		array.add(10);
+		BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+		bst.buildBinaryTree(array);
+		
+		assertFalse(bst.isAllEvenNumber());
+	}
+	@Test
+	public void testIsAllEvenNodes_Various_1()
+	{
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		array.add(30);
+		array.add(22);
+		array.add(44);
+		array.add(6);
+		array.add(46);
+		array.add(50);
+		array.add(26);
+		array.add(10);
+		BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+		bst.buildBinaryTree(array);
+		
+		assertTrue(bst.isAllEvenNumber());
+	}
+	@Test
+	public void testMaxNode()
+	{
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		array.add(30);
+		array.add(22);
+		array.add(44);
+		array.add(6);
+		array.add(46);
+		array.add(50);
+		array.add(26);
+		array.add(10);
+		BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+		bst.buildBinaryTree(array);
+		
+		assertEquals(50, bst.maxNode());
+	}
+	@Test
+	public void testMinNode()
+	{
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		array.add(30);
+		array.add(22);
+		array.add(44);
+		array.add(6);
+		array.add(46);
+		array.add(50);
+		array.add(26);
+		array.add(10);
+		BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+		bst.buildBinaryTree(array);
+		
+		assertEquals(6, bst.minNode());
+	}
+	@Test
+	public void testPostOrDer()
+	{
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		array.add(15);
+		array.add(12);
+		array.add(9);
+		array.add(56);
+		array.add(1);
+		array.add(16);
+		array.add(19);
+		array.add(22);
+		array.add(3);
+		array.add(100);
+		array.add(2);
+		array.add(25);
+		BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+		bst.buildBinaryTree(array);
+		bst.printPostOrder();
+		//assertEquals(6, bst.minNode());
+	}
 }
